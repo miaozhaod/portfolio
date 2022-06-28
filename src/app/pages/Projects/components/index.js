4062b1c412ad2787a0089674eaca33865be74ac6
Miao Zhao <miao_zhao@yahoo.com>
2022-06-28 16:11:18 +1000

    Update by exporting AComosus

--- src/app/pages/Projects/components/index.js
+++ src/app/pages/Projects/components/index.js
@@ -3,3 +3,4 @@ export { default as PortfolioApp } from "./PortfolioApp";
 export { default as GreatPhermesia } from "./GreatPhermesia";
 export { default as InspireLiteracy } from "./InspireLiteracy";
 export { default as UxLg } from "./UxLg";
+export { default as AComosus } from "./AComosus";
