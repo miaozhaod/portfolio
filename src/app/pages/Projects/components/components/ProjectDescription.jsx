18156d2369db33b3b068cd356a30cda67b902bed
Miao Zhao <miao_zhao@yahoo.com>
2022-09-17 21:34:55 +1000

    Update bg-color

--- src/app/pages/Projects/components/components/ProjectDescription.jsx
+++ src/app/pages/Projects/components/components/ProjectDescription.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function ProjectDescription({ children }) {
   return (
-    <div className="flex flex-col gap-3 py-6 px-4 bg-gray-100 ">{children}</div>
+    <div className="flex flex-col gap-3 py-6 px-4 bg-white ">{children}</div>
   );
 }
