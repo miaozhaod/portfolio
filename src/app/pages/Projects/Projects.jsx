32fdf432556a7ac51efab6948707425573f05c59
Miao Zhao <miao_zhao@yahoo.com>
2022-09-17 21:43:50 +1000

    Archive LG and InspireLiteracy projects

--- src/app/pages/Projects/Projects.jsx
+++ src/app/pages/Projects/Projects.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import {
-  WeatherApp,
-  InspireLiteracy,
-  UxLg,
-  AComosus,
-  PixelFoto,
-  ThinkLe,
-} from "./components";
+import { WeatherApp, AComosus, PixelFoto, ThinkLe } from "./components";
 import { Container, ScrollToTopButton } from "app/components";
 
 export default function Projects() {
-  const projects = [
-    <ThinkLe />,
-    <PixelFoto />,
-    <AComosus />,
-    <WeatherApp />,
-    <InspireLiteracy />,
-    <UxLg />,
-  ];
+  const projects = [<ThinkLe />, <PixelFoto />, <AComosus />, <WeatherApp />];
 
   return (
     <Container>
--- src/app/pages/Projects/components/index.js
+++ src/app/pages/Projects/components/index.js
@@ -1,8 +1,8 @@
 export { default as WeatherApp } from "./WeatherApp";
 export { default as PortfolioApp } from "./archive/PortfolioApp";
 export { default as GreatPhermesia } from "./archive/GreatPhermesia";
-export { default as InspireLiteracy } from "./InspireLiteracy";
-export { default as UxLg } from "./UxLg";
+export { default as InspireLiteracy } from "./archive/InspireLiteracy";
+export { default as UxLg } from "./archive/UxLg";
 export { default as AComosus } from "./AComosus";
 export { default as PixelFoto } from "./PixelFoto";
 export { default as ThinkLe } from "./ThinkLe";
