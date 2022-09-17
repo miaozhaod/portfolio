6fdba1c09b87161f8554c24de47c806bb0e259b4
Miao Zhao <miao_zhao@yahoo.com>
2022-09-17 19:20:16 +1000

    Update profession wording

--- src/app/pages/Home/components/Profession.jsx
+++ src/app/pages/Home/components/Profession.jsx
@@ -24,19 +24,28 @@ export default function Profession() {
           <ListCategory>WHAT I CAN DO</ListCategory>
           <ListItem>
             I worked on most of my front-end web projects through{" "}
-            <Highlighter>ReactJS, NextJs</Highlighter>
+            <Highlighter>ReactJS, NextJS</Highlighter>
           </ListItem>
           <ListItem>
             I am growing my skills in the backend by working on some
-            side-projects in <Highlighter>NodeJS, NestJS</Highlighter>,
+            side-projects in <Highlighter>NodeJS - Express, NestJS</Highlighter>
           </ListItem>
           <ListItem>
-            I am constantly exploring new technologies, recently I'm looking
-            into <Highlighter>serverless</Highlighter>.
+            I write <Highlighter>RESTful API, GraphQL API</Highlighter>
           </ListItem>
           <ListItem>
-            I have a three-year background in <Highlighter>UX</Highlighter>,
-            which challenges me to always keep users centred
+            I am constantly exploring new technologies, recently looking into{" "}
+            <Highlighter>serverless</Highlighter>.
+          </ListItem>
+          <ListItem>
+            I have a three-year background in{" "}
+            <Highlighter>UX design</Highlighter>, which challenges me to always
+            keep users centred
+          </ListItem>
+          <ListCategory>WHERE I WANT TO BE</ListCategory>
+          <ListItem>
+            I am looking forward to my next career challenge as a{" "}
+            <Highlighter>full-stack developer</Highlighter>.
           </ListItem>
         </List>
       </Details>
