5d3197818bcb99412359a73957656752887c1b93
Miao Zhao <miao_zhao@yahoo.com>
2022-09-19 21:32:37 +1000

    Add some more artworks

--- src/app/pages/Artworks/photos.js
+++ src/app/pages/Artworks/photos.js
@@ -8,6 +8,10 @@ import SheepBlue from "app/assets/arts/sheep_blue.jpg";
 import SheepGreen from "app/assets/arts/sheep_green.jpg";
 import SheepYellow from "app/assets/arts/sheep_yellow.jpg";
 import Sky from "app/assets/arts/Sky.png";
+import CuanDixia from "app/assets/arts/CuanDiXia.png";
+import GongYe from "app/assets/arts/GongYe.png";
+import Sweet from "app/assets/arts/Sweet.png";
+import NongJiaLe from "app/assets/arts/NongJiaLe.png";
 
 const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
 
@@ -22,6 +26,10 @@ const resources = [
   { src: SheepBlue, width: 1080, height: 1080 },
   { src: Panda, width: 1080, height: 1524 },
   { src: SheepYellow, width: 1080, height: 1080 },
+  { src: CuanDixia, width: 1080, height: 765 },
+  { src: GongYe, width: 1080, height: 1524 },
+  { src: Sweet, width: 1080, height: 765 },
+  { src: NongJiaLe, width: 1080, height: 765 },
 ];
 
 const photos = resources.map((photo) => ({
